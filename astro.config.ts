0b3991c36e0dadf18f6c4917163a6ad90ec1bb47
Declan L. Scott <declanlscott@gmail.com>
2024-03-29 16:31:30 -0500

    refactor: generate route tree with vite plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import react from "@astrojs/react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import aws from "astro-sst";
 import stylex from "astro-stylex";
 import { defineConfig } from "astro/config";
@@ -8,4 +9,7 @@ export default defineConfig({
   output: "hybrid",
   adapter: aws(),
   integrations: [react(), stylex()],
+  vite: {
+    plugins: [TanStackRouterVite()],
+  },
 });
