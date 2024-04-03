27fc85061f84f7efca7f7ad63d76b64350d36601
Declan L. Scott <declanlscott@gmail.com>
2024-04-03 17:58:50 -0500

    feat: entra id application

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-
+import * as entraId from "@pulumi/azuread";
 import env, { buildLocalDatabaseUrl } from "env";
 
 import { ClientPrefix } from "~/lib/client-resource";
@@ -25,6 +25,23 @@ export default $config({
     };
   },
   async run() {
+    const time = await import("@pulumiverse/time");
+
+    $linkable(entraId.Application, function () {
+      return {
+        properties: {
+          clientId: this.clientId,
+        },
+      };
+    });
+    $linkable(entraId.ApplicationPassword, function () {
+      return {
+        properties: {
+          value: this.value,
+        },
+      };
+    });
+
     const localDatabaseUrl = new sst.Secret(
       "LocalDatabaseUrl",
       buildLocalDatabaseUrl(),
@@ -35,12 +52,35 @@ export default $config({
       `${ClientPrefix}ReplicacheLicenseKey`,
     );
 
-    // new entraId.ApplicationRegistration("PaperwaitAppRegistration", {
-    //   displayName: "Paperwait",
-    // });
+    const entraIdApp = new entraId.Application("EntraIdApplication", {
+      displayName: "Paperwait",
+    });
+
+    const rotationHours = 24 * 7 * 26; // 6 months
+    const clientSecretRotation = new time.Rotating("ClientSecretRotation", {
+      rotationHours,
+    });
+    const entraIdClientSecret = new entraId.ApplicationPassword(
+      "EntraIdClientSecret",
+      {
+        applicationObjectId: entraIdApp.id,
+        endDateRelative: `${rotationHours.toString()}h`,
+        rotateWhenChanged: {
+          rotation: clientSecretRotation.id,
+        },
+      },
+    );
+    new entraId.ServicePrincipal("EntraIdServicePrincipal", {
+      clientId: entraIdApp.clientId,
+    });
 
     new sst.aws.Astro("Paperwait", {
-      link: [replicacheLicenseKey, $dev ? localDatabaseUrl : remoteDatabaseUrl],
+      link: [
+        replicacheLicenseKey,
+        $dev ? localDatabaseUrl : remoteDatabaseUrl,
+        entraIdApp,
+        entraIdClientSecret,
+      ],
     });
   },
 });
