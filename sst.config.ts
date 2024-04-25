490aa6b63997ba9f878aaeb02daf3b77fd3d558f
Declan L. Scott <declanlscott@gmail.com>
2024-04-24 20:05:12 -0500

    chore: script for injecting env vars into partykit
    NOTE: make sure to run partykit through `sst shell`

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,10 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 import { AWS_REGION } from "@paperwait/core/constants";
-import env from "env";
+
+const AWS_ORG_NAME = process.env.AWS_ORG_NAME;
+if (!AWS_ORG_NAME) {
+  throw new Error("AWS_ORG_NAME is not set");
+}
 
 export default $config({
   app(input) {
@@ -12,8 +16,8 @@ export default $config({
         aws: {
           profile:
             input.stage === "production"
-              ? `${env.AWS_ORG_NAME}-production`
-              : `${env.AWS_ORG_NAME}-dev`,
+              ? `${AWS_ORG_NAME}-production`
+              : `${AWS_ORG_NAME}-dev`,
           region: AWS_REGION,
         },
       },
