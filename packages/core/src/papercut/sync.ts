eaeb2263f18a79b691195c15de19b436b4b16b06
Declan L. Scott <declanlscott@gmail.com>
2024-06-06 15:22:54 -0500

    refactor: papercut accounts sync

--- packages/core/src/papercut/sync.ts
+++ packages/core/src/papercut/sync.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { and, eq, inArray, isNull } from "drizzle-orm";
 
 import { getSharedAccountPropertiesOutputIndex } from "../constants";
 import { buildConflictUpdateColumns } from "../drizzle/columns";
@@ -23,13 +23,31 @@ export async function syncPapercutAccounts(
   orgId: Organization["id"],
   _args: SyncPapercutAccountsMutationArgs,
 ) {
-  const allNames = await listSharedAccounts({ orgId, input: {} });
-  if (allNames.length === 0) return;
+  const names = await listSharedAccounts({ orgId, input: {} });
 
-  const [allSharedAccounts, allUsers] = await Promise.all([
-    // Fetch all shared accounts
+  const [
+    prevPapercutAccounts,
+    nextPapercutAccounts,
+    users,
+    prevPapercutAccountCustomerAuthorizations,
+  ] = await Promise.all([
+    // Fetch previous papercut accounts
+    tx
+      .select({
+        id: PapercutAccount.id,
+        name: PapercutAccount.name,
+        orgId: PapercutAccount.orgId,
+      })
+      .from(PapercutAccount)
+      .where(
+        and(
+          eq(PapercutAccount.orgId, orgId),
+          isNull(PapercutAccount.deletedAt),
+        ),
+      ),
+    // Fetch next papercut accounts
     Promise.all(
-      allNames.map(async (name) => {
+      names.map(async (name) => {
         const properties = await getSharedAccountProperties({
           orgId,
           input: { sharedAccountName: name },
@@ -49,59 +67,181 @@ export async function syncPapercutAccounts(
       .select({ id: User.id, username: User.username })
       .from(User)
       .where(eq(User.orgId, orgId)),
-  ]);
-  if (allSharedAccounts.length === 0) return;
-
-  const [customerAuthorizations] = await Promise.all([
-    // Build the customer authorization entries
-    Promise.all(
-      allUsers.map(async (user) => {
-        const names = await listUserSharedAccounts({
-          orgId,
-          input: {
-            username: user.username,
-            ignoreUserAccountSelectionConfig: true,
-          },
-        });
-
-        const sharedAccounts = names
-          .map((name) =>
-            allSharedAccounts.find(
-              (sharedAccount) => sharedAccount.name === name,
-            ),
-          )
-          .filter(Boolean);
-
-        return sharedAccounts.map(
-          (sharedAccount) =>
-            ({
-              orgId,
-              customerId: user.id,
-              papercutAccountId: sharedAccount.id,
-            }) satisfies Omit<
-              OmitTimestamps<PapercutAccountCustomerAuthorization>,
-              "id"
-            >,
-        );
-      }),
-    ).then((result) => result.flat()),
-    // Upsert the papercut accounts
+    // Fetch previous papercut account customer authorizations
     tx
-      .insert(PapercutAccount)
-      .values(allSharedAccounts)
-      .onConflictDoUpdate({
-        target: [PapercutAccount.id, PapercutAccount.orgId],
-        set: {
-          ...buildConflictUpdateColumns(PapercutAccount, ["name"]),
-          updatedAt: new Date().toISOString(),
-        },
-      }),
+      .select({
+        id: PapercutAccountCustomerAuthorization.id,
+        customerId: PapercutAccountCustomerAuthorization.customerId,
+        papercutAccountId:
+          PapercutAccountCustomerAuthorization.papercutAccountId,
+        orgId: PapercutAccountCustomerAuthorization.orgId,
+      })
+      .from(PapercutAccountCustomerAuthorization)
+      .where(
+        and(
+          eq(PapercutAccountCustomerAuthorization.orgId, orgId),
+          isNull(PapercutAccountCustomerAuthorization.deletedAt),
+        ),
+      ),
   ]);
-  if (customerAuthorizations.length === 0) return;
 
-  // Insert the customer authorizations
-  await tx
-    .insert(PapercutAccountCustomerAuthorization)
-    .values(customerAuthorizations)
-    .onConflictDoNothing();
+  // Calculate diff
+  const papercutAccountsDiff = diffPapercutAccounts(
+    prevPapercutAccounts,
+    nextPapercutAccounts,
+  );
+
+  // Build next papercut account customer authorizations
+  const nextPapercutAccountCustomerAuthorizations = await Promise.all(
+    users.map(async (user) => {
+      const names = await listUserSharedAccounts({
+        orgId,
+        input: {
+          username: user.username,
+          ignoreUserAccountSelectionConfig: true,
+        },
+      });
+
+      const userSharedAccounts = names
+        .map((name) =>
+          nextPapercutAccounts.find(
+            (papercutAccount) => papercutAccount.name === name,
+          ),
+        )
+        .filter(Boolean);
+
+      return userSharedAccounts.map(
+        ({ id: papercutAccountId }) =>
+          ({
+            orgId,
+            customerId: user.id,
+            papercutAccountId,
+          }) satisfies Omit<
+            OmitTimestamps<PapercutAccountCustomerAuthorization>,
+            "id"
+          >,
+      );
+    }),
+  ).then((result) => result.flat());
+
+  // Calculate diff
+  const papercutAccountCustomerAuthorizationsDiff =
+    diffPapercutAccountCustomerAuthorizations(
+      prevPapercutAccountCustomerAuthorizations,
+      nextPapercutAccountCustomerAuthorizations,
+    );
+
+  await Promise.all([
+    // Insert papercut accounts
+    papercutAccountsDiff.puts.length &&
+      tx
+        .insert(PapercutAccount)
+        .values(papercutAccountsDiff.puts)
+        .onConflictDoUpdate({
+          target: [PapercutAccount.id, PapercutAccount.orgId],
+          set: {
+            ...buildConflictUpdateColumns(PapercutAccount, ["name"]),
+            updatedAt: new Date().toISOString(),
+            deletedAt: null,
+          },
+        }),
+    // Soft delete papercut accounts
+    papercutAccountsDiff.dels.length &&
+      tx
+        .update(PapercutAccount)
+        .set({ deletedAt: new Date().toISOString() })
+        .where(
+          and(
+            inArray(PapercutAccount.id, papercutAccountsDiff.dels),
+            eq(PapercutAccount.orgId, orgId),
+          ),
+        ),
+    // Insert customer authorizations
+    papercutAccountCustomerAuthorizationsDiff.puts.length &&
+      tx
+        .insert(PapercutAccountCustomerAuthorization)
+        .values(papercutAccountCustomerAuthorizationsDiff.puts)
+        .onConflictDoUpdate({
+          target: [
+            PapercutAccountCustomerAuthorization.papercutAccountId,
+            PapercutAccountCustomerAuthorization.customerId,
+          ],
+          set: {
+            updatedAt: new Date().toISOString(),
+            deletedAt: null,
+          },
+        }),
+    // Soft delete customer authorizations
+    papercutAccountCustomerAuthorizationsDiff.dels.length &&
+      tx
+        .update(PapercutAccountCustomerAuthorization)
+        .set({ deletedAt: new Date().toISOString() })
+        .where(
+          and(
+            eq(
+              inArray(
+                PapercutAccountCustomerAuthorization.id,
+                papercutAccountCustomerAuthorizationsDiff.dels,
+              ),
+              eq(PapercutAccountCustomerAuthorization.orgId, orgId),
+            ),
+          ),
+        ),
+  ]);
+}
+
+type PapercutAccountsDiff = {
+  puts: Array<OmitTimestamps<PapercutAccount>>;
+  dels: Array<PapercutAccount["id"]>;
+};
+
+function diffPapercutAccounts(
+  prev: Array<OmitTimestamps<PapercutAccount>>,
+  next: Array<OmitTimestamps<PapercutAccount>>,
+) {
+  return {
+    puts: next.filter(
+      (nextAccount) =>
+        !prev.some((prevAccount) => prevAccount.id === nextAccount.id),
+    ),
+    dels: prev
+      .filter(
+        (prevAccount) =>
+          !next.some((nextAccount) => nextAccount.id === prevAccount.id),
+      )
+      .map((entry) => entry.id),
+  } satisfies PapercutAccountsDiff;
+}
+
+type PapercutAccountCustomerAuthorizationsDiff = {
+  puts: Array<Omit<OmitTimestamps<PapercutAccountCustomerAuthorization>, "id">>;
+  dels: Array<PapercutAccountCustomerAuthorization["id"]>;
+};
+
+function diffPapercutAccountCustomerAuthorizations(
+  prev: Array<OmitTimestamps<PapercutAccountCustomerAuthorization>>,
+  next: Array<Omit<OmitTimestamps<PapercutAccountCustomerAuthorization>, "id">>,
+) {
+  return {
+    puts: next.filter(
+      (nextAuthorization) =>
+        !prev.some(
+          (prevAuthorization) =>
+            prevAuthorization.customerId === nextAuthorization.customerId &&
+            prevAuthorization.papercutAccountId ===
+              nextAuthorization.papercutAccountId,
+        ),
+    ),
+    dels: prev
+      .filter(
+        (prevAuthorization) =>
+          !next.some(
+            (nextAuthorization) =>
+              nextAuthorization.customerId === prevAuthorization.customerId &&
+              nextAuthorization.papercutAccountId ===
+                prevAuthorization.papercutAccountId,
+          ),
+      )
+      .map(({ id }) => id),
+  } satisfies PapercutAccountCustomerAuthorizationsDiff;
 }
