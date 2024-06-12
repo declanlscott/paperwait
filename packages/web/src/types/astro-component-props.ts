0b5f147cacda4e78d945d18b60d977fa467de76c
Declan L. Scott <declanlscott@gmail.com>
2024-06-12 10:42:19 -0500

    fix: astro component types

---
+++ packages/web/src/types/astro-component-props.ts
@@ -0,0 +1,32 @@
+import type { Provider } from "@paperwait/core/organization";
+
+export type OrgProviderIdProps = {
+  provider?: Provider;
+};
+
+export type OrgSlugProps = {
+  value?: string;
+  isValid?: boolean;
+};
+
+export type PapercutAuthTokenProps = {
+  value?: string;
+  show?: boolean;
+};
+
+export type PapercutTestProps =
+  | { status: "initial" }
+  | { status: "success" }
+  | { status: "error"; reason: string };
+
+export type AppLoadingIndicatorProps = {
+  isInitial?: boolean;
+};
+
+export type LoadingIndicatorProps = {
+  color?: string;
+};
+
+export type LogoProps = {
+  color?: string;
+};
