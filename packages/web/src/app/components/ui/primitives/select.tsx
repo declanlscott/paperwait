8df651586454ad8e0ca7689d4b741e98eb786be0
Declan L. Scott <declanlscott@gmail.com>
2024-06-18 23:00:04 -0500

    feat: select component

---
+++ packages/web/src/app/components/ui/primitives/select.tsx
@@ -0,0 +1,140 @@
+import {
+  Button as AriaButton,
+  Collection as AriaCollection,
+  Header as AriaHeader,
+  ListBox as AriaListBox,
+  Popover as AriaPopover,
+  Section as AriaSection,
+  Select as AriaSelect,
+  ListBoxItem as AriaSelectBoxItem,
+  SelectValue as AriaSelectValue,
+  Separator as AriaSeparator,
+  composeRenderProps,
+} from "react-aria-components";
+import { Check, ChevronDown } from "lucide-react";
+
+import {
+  selectContentStyles,
+  selectHeaderStyles,
+  selectItemStyles,
+  selectPopoverStyles,
+  selectSeparatorStyles,
+  selectTriggerStyles,
+  selectValueStyles,
+} from "~/shared/styles/components/primitives/select";
+
+import type { ComponentProps } from "react";
+import type {
+  ButtonProps as AriaButtonProps,
+  ListBoxItemProps as AriaListBoxItemProps,
+  ListBoxProps as AriaListBoxProps,
+  PopoverProps as AriaPopoverProps,
+  SelectValueProps as AriaSelectValueProps,
+  SeparatorProps as AriaSeparatorProps,
+} from "react-aria-components";
+
+export const Select = AriaSelect;
+
+export const SelectSection = AriaSection;
+
+export const SelectCollection = AriaCollection;
+
+export type SelectValueProps<T extends object> = AriaSelectValueProps<T>;
+export const SelectValue = <T extends object>({
+  className,
+  ...props
+}: SelectValueProps<T>) => (
+  <AriaSelectValue
+    className={composeRenderProps(className, (className, renderProps) =>
+      selectValueStyles({ ...renderProps, className }),
+    )}
+    {...props}
+  />
+);
+
+export type SelectTriggerProps = AriaButtonProps;
+export const SelectTrigger = ({
+  className,
+  children,
+  ...props
+}: SelectTriggerProps) => (
+  <AriaButton
+    className={composeRenderProps(className, (className, renderProps) =>
+      selectTriggerStyles({ ...renderProps, className }),
+    )}
+    {...props}
+  >
+    {(values) => (
+      <>
+        {typeof children === "function" ? children(values) : children}
+        <ChevronDown aria-hidden="true" className="h-4 w-4 opacity-50" />
+      </>
+    )}
+  </AriaButton>
+);
+
+export type SelectHeaderProps = ComponentProps<typeof AriaHeader>;
+export const SelectHeader = ({ className, ...props }: SelectHeaderProps) => (
+  <AriaHeader className={selectHeaderStyles({ className })} {...props} />
+);
+
+export type SelectItemProps = AriaListBoxItemProps;
+export const SelectItem = ({
+  className,
+  children,
+  ...props
+}: SelectItemProps) => (
+  <AriaSelectBoxItem
+    className={composeRenderProps(className, (className, renderProps) =>
+      selectItemStyles({ ...renderProps, className }),
+    )}
+    {...props}
+  >
+    {(values) => (
+      <>
+        {values.isSelected && (
+          <span className="absolute left-2 flex h-4 w-4 items-center justify-center">
+            <Check className="h-4 w-4" />
+          </span>
+        )}
+        {typeof children === "function" ? children(values) : children}
+      </>
+    )}
+  </AriaSelectBoxItem>
+);
+
+export type SelectSeparatorProps = AriaSeparatorProps;
+export const SelectSeparator = ({
+  className,
+  ...props
+}: SelectSeparatorProps) => (
+  <AriaSeparator className={selectSeparatorStyles({ className })} {...props} />
+);
+
+export type SelectPopoverProps = AriaPopoverProps;
+export const SelectPopover = ({
+  className,
+  offset = 0,
+  ...props
+}: SelectPopoverProps) => (
+  <AriaPopover
+    offset={offset}
+    className={composeRenderProps(className, (className, renderProps) =>
+      selectPopoverStyles({ ...renderProps, className }),
+    )}
+    {...props}
+  />
+);
+
+export type SelectContentProps<T extends object> = AriaListBoxProps<T>;
+export const SelectContent = <T extends object>({
+  className,
+  ...props
+}: SelectContentProps<T>) => (
+  <AriaListBox
+    className={composeRenderProps(className, (className, renderProps) =>
+      selectContentStyles({ ...renderProps, className }),
+    )}
+    {...props}
+  />
+);
--- packages/web/src/shared/styles/components/primitives/select.ts
+++ packages/web/src/shared/styles/components/primitives/select.ts
@@ -1,5 +1,7 @@
 import { tv } from "tailwind-variants";
 
+import { focusRing } from "~/shared/styles/utils";
+
 import type { VariantProps } from "tailwind-variants";
 
 export const selectStyles = tv({
@@ -10,5 +12,72 @@ export const selectStyles = tv({
     },
   },
 });
-
 export type SelectStyles = VariantProps<typeof selectStyles>;
+
+export const selectValueStyles = tv({
+  variants: {
+    isPlaceholder: {
+      true: "text-muted-foreground",
+    },
+  },
+});
+export type SelectValueStyles = VariantProps<typeof selectValueStyles>;
+
+export const selectTriggerStyles = tv({
+  extend: focusRing,
+  base: "border-input bg-background flex h-10 w-full items-center justify-between rounded-md border px-3 py-2 text-sm",
+  variants: {
+    isDisabled: {
+      true: "cursor-not-allowed opacity-50",
+    },
+  },
+});
+export type SelectTriggerStyles = VariantProps<typeof selectTriggerStyles>;
+
+export const selectHeaderStyles = tv({
+  base: "py-1.5 pl-8 pr-2 text-sm font-semibold",
+});
+export type SelectHeaderStyles = VariantProps<typeof selectHeaderStyles>;
+
+export const selectItemStyles = tv({
+  base: "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none",
+  variants: {
+    isFocused: {
+      true: "bg-accent text-accent-foreground",
+    },
+    isDisabled: {
+      true: "pointer-events-none opacity-50",
+    },
+  },
+});
+export type SelectItemStyles = VariantProps<typeof selectItemStyles>;
+
+export const selectSeparatorStyles = tv({
+  base: "-mx-1 my-1 h-px bg-muted",
+});
+export type SelectSeparatorStyles = VariantProps<typeof selectSeparatorStyles>;
+
+export const selectPopoverStyles = tv({
+  base: "relative z-50 w-[--trigger-width]  min-w-[8rem] overflow-y-auto rounded-md border bg-popover text-popover-foreground shadow-md",
+  variants: {
+    isEntering: {
+      true: "animate-in fade-in-0",
+    },
+    isExiting: {
+      true: "animate-out fade-out-0 zoom-out-95",
+    },
+    placement: {
+      bottom: "slide-in-from-top-2 translate-y-1",
+      left: "slide-in-from-right-2 -translate-x-1",
+      right: "slide-in-from-left-2 translate-x-1",
+      top: "slide-in-from-bottom-2 -translate-y-1",
+      center: "",
+    },
+  },
+});
+export type SelectPopoverStyles = VariantProps<typeof selectPopoverStyles>;
+
+export const selectContentStyles = tv({
+  base: "p-1",
+});
+export type SelectContentStyles = VariantProps<typeof selectContentStyles>;
