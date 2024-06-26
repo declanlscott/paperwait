b8b1087cea24fd26fb1d05fc003f4110e4bdbf20
Declan L. Scott <declanlscott@gmail.com>
2024-06-25 23:14:57 -0500

    feat: button loading indicator

--- packages/web/src/app/components/ui/primitives/button.tsx
+++ packages/web/src/app/components/ui/primitives/button.tsx
@@ -2,18 +2,27 @@ import {
   Button as AriaButton,
   composeRenderProps,
 } from "react-aria-components";
+import { Loader2 } from "lucide-react";
 
 import { buttonStyles } from "~/shared/styles/components/primitives/button";
 
 import type { ButtonProps as AriaButtonProps } from "react-aria-components";
 import type { ButtonStyles } from "~/shared/styles/components/primitives/button";
 
-export interface ButtonProps extends AriaButtonProps, ButtonStyles {}
+export interface ButtonProps extends AriaButtonProps, ButtonStyles {
+  isLoading?: boolean;
+}
 
-export function Button(props: ButtonProps) {
+export function Button({
+  children,
+  isDisabled,
+  isLoading,
+  ...props
+}: ButtonProps) {
   return (
     <AriaButton
       {...props}
+      isDisabled={isLoading ?? isDisabled}
       className={composeRenderProps(props.className, (className, renderProps) =>
         buttonStyles({
           ...renderProps,
@@ -22,6 +31,14 @@ export function Button(props: ButtonProps) {
           className,
         }),
       )}
-    />
+    >
+      {(values) => (
+        <>
+          {isLoading ? <Loader2 className="mr-2 size-4 animate-spin" /> : null}
+
+          {typeof children === "function" ? children(values) : children}
+        </>
+      )}
+    </AriaButton>
   );
 }
