eefb7019c372d07208682a74dc6657d0afecc823
Declan L. Scott <declanlscott@gmail.com>
2024-07-16 14:35:57 -0500

    fix: redirect to `/dashboard` when authed
    https://www.github.com/TanStack/router/pull/1907 was merged

--- packages/web/src/app/routes/login.tsx
+++ packages/web/src/app/routes/login.tsx
@@ -14,8 +14,7 @@ export const Route = createFileRoute("/login")({
   validateSearch: (search) =>
     validate(v.fallback(LoginSearchParams, initialLoginSearchParams), search),
   beforeLoad: ({ context }) => {
-    // Uncomment when https://www.github.com/TanStack/router/pull/1907 is merged
-    // if (context.authStore.user) throw redirect({ to: "/dashboard" });
+    if (context.authStore.user) throw redirect({ to: "/dashboard" });
   },
   component: Component,
 });
