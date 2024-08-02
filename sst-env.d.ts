d6f7754a93ff68866d1a2be9cd1b646a6cbbc5b4
Declan L. Scott <declanlscott@gmail.com>
2024-08-02 16:42:53 -0500

    chore: bump deps

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,114 +3,114 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    AdjustSharedAccountAccountBalanceDeadLetterQueue: {
-      type: "sst.aws.Queue"
-      url: string
+    "AdjustSharedAccountAccountBalanceDeadLetterQueue": {
+      "type": "sst.aws.Queue"
+      "url": string
     }
-    AdjustSharedAccountAccountBalanceQueue: {
-      type: "sst.aws.Queue"
-      url: string
+    "AdjustSharedAccountAccountBalanceQueue": {
+      "type": "sst.aws.Queue"
+      "url": string
     }
-    AssetsBucket: {
-      name: string
-      type: "sst.aws.Bucket"
+    "AssetsBucket": {
+      "name": string
+      "type": "sst.aws.Bucket"
     }
-    AssetsDistribution: {
-      domainName: string
-      type: "aws.cloudfront/distribution.Distribution"
+    "AssetsDistribution": {
+      "domainName": string
+      "type": "aws.cloudfront/distribution.Distribution"
     }
-    AssetsDistributionPrivateKey: {
-      privateKeyPem: string
-      type: "tls.index/privateKey.PrivateKey"
+    "AssetsDistributionPrivateKey": {
+      "privateKeyPem": string
+      "type": "tls.index/privateKey.PrivateKey"
     }
-    AssetsDistributionPublicKey: {
-      encodedKey: string
-      id: string
-      type: "aws.cloudfront/publicKey.PublicKey"
+    "AssetsDistributionPublicKey": {
+      "encodedKey": string
+      "id": string
+      "type": "aws.cloudfront/publicKey.PublicKey"
     }
-    ClientDomain: {
-      type: "sst.sst.Secret"
-      value: string
+    "ClientDomain": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    ClientIsDev: {
-      type: "sst.sst.Secret"
-      value: string
+    "ClientIsDev": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    ClientPartyKitUrl: {
-      type: "sst.sst.Secret"
-      value: string
+    "ClientPartyKitUrl": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    ClientReplicacheLicenseKey: {
-      type: "sst.sst.Secret"
-      value: string
+    "ClientReplicacheLicenseKey": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    DocumentsBucket: {
-      name: string
-      type: "sst.aws.Bucket"
+    "DocumentsBucket": {
+      "name": string
+      "type": "sst.aws.Bucket"
     }
-    EntraIdApplication: {
-      clientId: string
-      type: "azuread.index/application.Application"
+    "EntraIdApplication": {
+      "clientId": string
+      "type": "azuread.index/application.Application"
     }
-    EntraIdClientSecret: {
-      type: "azuread.index/applicationPassword.ApplicationPassword"
-      value: string
+    "EntraIdClientSecret": {
+      "type": "azuread.index/applicationPassword.ApplicationPassword"
+      "value": string
     }
-    GoogleClientId: {
-      type: "sst.sst.Secret"
-      value: string
+    "GoogleClientId": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    GoogleClientSecret: {
-      type: "sst.sst.Secret"
-      value: string
+    "GoogleClientSecret": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    MockPapercutApi: {
-      type: "sst.cloudflare.Worker"
-      url: string
+    "MockPapercutApi": {
+      "type": "sst.cloudflare.Worker"
+      "url": string
     }
-    NatInstance: {
-      publicIp: string
-      type: "aws.ec2/instance.Instance"
+    "NatInstance": {
+      "publicIp": string
+      "type": "aws.ec2/instance.Instance"
     }
-    NatSshKey: {
-      privateKeyPem: string
-      type: "tls.index/privateKey.PrivateKey"
+    "NatSshKey": {
+      "privateKeyPem": string
+      "type": "tls.index/privateKey.PrivateKey"
     }
-    PapercutApiGateway: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
+    "PapercutApiGateway": {
+      "type": "sst.aws.ApiGatewayV2"
+      "url": string
     }
-    Paperwait: {
-      type: "sst.aws.Astro"
-      url: string
+    "Paperwait": {
+      "type": "sst.aws.Astro"
+      "url": string
     }
-    PartyKitApiKey: {
-      type: "sst.sst.Secret"
-      value: string
+    "PartyKitApiKey": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresDatabase: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresDatabase": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresHost: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresHost": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresPassword: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresPassword": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresPort: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresPort": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresSsl: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresSsl": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    PostgresUser: {
-      type: "sst.sst.Secret"
-      value: string
+    "PostgresUser": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
   }
 }
