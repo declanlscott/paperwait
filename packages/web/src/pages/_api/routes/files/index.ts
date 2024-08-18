f820d2f2c0bbd01276a6b5efb15449d0c37d3f0a
Declan L. Scott <declanlscott@gmail.com>
2024-08-18 11:52:16 -0500

    feat(api): max file sizes GET endpoint

--- packages/web/src/pages/_api/routes/files/index.ts
+++ packages/web/src/pages/_api/routes/files/index.ts
@@ -1,3 +1,8 @@
+import { buildSsmParameterPath, getSsmParameter } from "@paperwait/core/aws";
+import { MAX_FILE_SIZES_PARAMETER_NAME } from "@paperwait/core/constants";
+import { InternalServerError } from "@paperwait/core/errors";
+import { MaxFileSizes } from "@paperwait/core/schemas";
+import { validate } from "@paperwait/core/valibot";
 import { Hono } from "hono";
 
 import { authorization } from "~/api/middleware";
@@ -8,5 +13,25 @@ import type { HonoEnv } from "~/api/types";
 
 export default new Hono<HonoEnv>()
   .use(authorization())
+  .get(
+    "/max-file-sizes",
+    authorization(["administrator", "operator"]),
+    async (c) => {
+      const orgId = c.env.locals.org!.id;
+
+      const maxFileSizes = validate(
+        MaxFileSizes,
+        await getSsmParameter({
+          Name: buildSsmParameterPath(orgId, MAX_FILE_SIZES_PARAMETER_NAME),
+        }),
+        {
+          Error: InternalServerError,
+          message: "Failed to get max file sizes",
+        },
+      );
+
+      return c.json(maxFileSizes, 200);
+    },
+  )
   .route("/assets", assets)
   .route("/documents", documents);
