a96ba32485f73113c32a3bf1103091d5189922f8
Declan L. Scott <declanlscott@gmail.com>
2024-09-07 15:21:51 -0500

    style: cloudfront `getSignedUrl` export

--- packages/core/src/aws/cloudfront.ts
+++ packages/core/src/aws/cloudfront.ts
@@ -1,4 +1,4 @@
-export { getSignedUrl as getCloudfrontSignedUrl } from "@aws-sdk/cloudfront-signer";
+export { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 
 export const buildUrl = (domainName: string, path: string) =>
   `https://${domainName}/${path}`;
