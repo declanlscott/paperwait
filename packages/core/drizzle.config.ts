2e3751026de29c55e86ecf2e0f065d1048acc4b3
Declan L. Scott <declanlscott@gmail.com>
2024-10-12 01:54:02 -0500

    refactor: remove drizzle relations

--- packages/core/drizzle.config.ts
+++ packages/core/drizzle.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { Resource } from "sst";
 
 export default defineConfig({
-  schema: [
-    "./src/drizzle/enums.sql.ts",
-    "./src/drizzle/relations.sql.ts",
-    "./src/**/sql.ts",
-  ],
+  schema: ["./src/**/*.sql.ts", "./src/**/sql.ts"],
   out: "./migrations/",
   dialect: "postgresql",
   dbCredentials: {
