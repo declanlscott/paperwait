f3a858de55e8e98f7f9795579e8f0a275dfc57e4
Declan L. Scott <declanlscott@gmail.com>
2024-11-11 16:21:28 -0600

    fix: rate limiter workers

--- packages/workers/ip-rate-limiter/src/index.ts
+++ packages/workers/ip-rate-limiter/src/index.ts
@@ -5,8 +5,11 @@ import type { RateLimit, RateLimitOptions } from "@cloudflare/workers-types";
 export default class extends WorkerEntrypoint<{
   IP_RATE_LIMITER: RateLimit;
 }> {
-  fetch = async () => new Response("Healthy!");
+  fetch() {
+    return new Response("Healthy!");
+  }
 
-  limit = async (options: RateLimitOptions) =>
-    this.env.IP_RATE_LIMITER.limit(options);
+  async limit(options: RateLimitOptions) {
+    return this.env.IP_RATE_LIMITER.limit(options);
+  }
 }
