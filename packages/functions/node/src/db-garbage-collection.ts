d95e5e9f689423d6d27be662684143d5b2a478b0
Declan L. Scott <declanlscott@gmail.com>
2024-12-13 23:51:27 -0600

    fix: remove sessions cleanup

--- packages/functions/node/src/db-garbage-collection.ts
+++ packages/functions/node/src/db-garbage-collection.ts
@@ -1,11 +1,9 @@
 import { Replicache } from "@printworks/core/replicache";
-import { Sessions } from "@printworks/core/sessions";
 
 import type { EventBridgeHandler } from "aws-lambda";
 
 export const handler: EventBridgeHandler<string, unknown, void> = async () => {
   await Promise.all([
-    Sessions.deleteExpired(),
     Replicache.deleteExpiredClientGroups(),
     Replicache.deleteExpiredClients(),
   ]);
