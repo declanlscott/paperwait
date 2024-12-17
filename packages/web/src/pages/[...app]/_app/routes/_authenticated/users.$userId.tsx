c1426a28b617d280901b5d4899befdcb3fe5da9c
Declan L. Scott <declanlscott@gmail.com>
2024-12-17 14:40:22 -0600

    fix: user route authorization (again, oops)

--- packages/web/src/pages/[...app]/_app/routes/_authenticated/users.$userId.tsx
+++ packages/web/src/pages/[...app]/_app/routes/_authenticated/users.$userId.tsx
@@ -3,13 +3,13 @@ import { createFileRoute } from "@tanstack/react-router";
 const routeId = "/_authenticated/users/$userId";
 
 export const Route = createFileRoute(routeId)({
-  beforeLoad: ({ context }) =>
+  beforeLoad: ({ context, params }) =>
     context.replicache.query((tx) =>
       context.auth.authorizeRoute(
         tx,
         context.actor.properties.id,
         routeId,
-        context.actor.properties.id,
+        params.userId,
       ),
     ),
   component: Component,
