35788544e6211a744ad159541fb38eb349750f41
Declan L. Scott <declanlscott@gmail.com>
2024-12-17 14:04:27 -0600

    refactor: user hooks

--- packages/web/src/pages/[...app]/_app/lib/hooks/user.ts
+++ packages/web/src/pages/[...app]/_app/lib/hooks/user.ts
@@ -3,7 +3,10 @@ import { usersTableName } from "@printworks/core/users/shared";
 import { ApplicationError } from "@printworks/core/utils/errors";
 
 import { useUserActor } from "~/app/lib/hooks/actor";
-import { useQuery } from "~/app/lib/hooks/data";
+import { queryFactory, useQuery } from "~/app/lib/hooks/data";
+
+import type { BillingAccount } from "@printworks/core/billing-accounts/sql";
+import type { User } from "@printworks/core/users/sql";
 
 export function useUser() {
   const { id } = useUserActor();
@@ -13,4 +16,22 @@ export function useUser() {
     throw new ApplicationError.EntityNotFound({ name: usersTableName, id });
 
   return user;
+}
+
+export function useManager() {
+  const user = useUser();
+
+  if (user.profile.role !== "manager")
+    throw new ApplicationError.AccessDenied();
+
+  const billingAccountIds = useQuery(
+    queryFactory.managedBillingAccountIds(user.id),
+    { defaultData: [] as Array<BillingAccount["id"]> },
+  );
+
+  const customerIds = useQuery(queryFactory.managedCustomerIds(user.id), {
+    defaultData: [] as Array<User["id"]>,
+  });
+
+  return { billingAccountIds, customerIds };
 }
